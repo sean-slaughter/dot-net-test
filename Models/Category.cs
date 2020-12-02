48f695e9c358dd3d71958b5b85581ddc3ed44e04
sean-slaughter <sthompson2822@gmail.com>
2020-12-01 19:10:47 -0600

    Add first model and dependencies

---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dot_net_mvc_test.Models 
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }        
+    }
+}
